eed46ae60a8da6a08bd95aa679079f7ced0ca6b4
rawsrc <rawsrc@gmail.com>
2021-04-13 11:54:16 +0200

    Grouping into regions and reordering stdPhpEchoHelpers.php

--- stdPhpEchoHelpers.php
+++ stdPhpEchoHelpers.php
@@ -6,48 +6,15 @@ use Execodis\PhpEcho\PhpEcho;
 
 $helpers = [];
 
-//region raw
-/**
- * Return the raw value from the key in parameter
- * CAREFUL : THIS VALUE IS NOT ESCAPED
- *
- * This helper is linked to an instance of PhpEcho
- *
- * Support the key space notation for array and sub-arrays
- * if $key = 'abc def' then the engine will search for
- * the value as $vars['abc']['def']
- *
- * @param string $key
- * @return mixed
- */
-$raw = function(string $key): mixed {
-    if (isset($this->vars[$key])) {
-        return $this->vars[$key];
-    } else {
-        $keys = explode(' ', $key);
-        $cursor = $this->vars;
-        foreach ($keys as $k) {
-            if (isset($cursor[$k])) {
-                $cursor = $cursor[$k];
-            } else {
-                return null;
-            }
-        }
-
-        return $cursor;
-    }
-};
-$helpers['raw'] = [$raw, HELPER_BOUND_TO_CLASS_INSTANCE, HELPER_RETURN_ESCAPED_DATA];
-//endregion
-
+//region STANDALONE HELPERS
 //region is_scalar
 /**
  * This is a standalone helper
  *
- * @param $p
+ * @param mixed $p
  * @return bool
  */
-$is_scalar = function($p): bool {
+$is_scalar = function(mixed $p): bool {
     return is_scalar($p) || (is_object($p) && method_exists($p, '__toString'));
 };
 $helpers['isScalar'] = [$is_scalar, HELPER_RETURN_ESCAPED_DATA];
@@ -57,10 +24,10 @@ $helpers['isScalar'] = [$is_scalar, HELPER_RETURN_ESCAPED_DATA];
 /**
  * Check if the value in parameter should be escaped or not
  *
- * @param $p
+ * @param mixed $p
  * @return bool
  */
-$to_escape = function($p) use ($is_scalar): bool {
+$to_escape = function(mixed $p) use ($is_scalar): bool  {
     if (is_string($p)) {
         return true;
     } elseif (is_bool($p) || is_int($p) || is_float($p) || ($p instanceof PhpEcho)) {
@@ -121,7 +88,7 @@ $hsc_array = function(array $part) use (&$hsc_array, $to_escape): array {
  * @param mixed $p
  * @return array|string
  */
-$hsc = function($p) use ($hsc_array, $is_scalar): array|string {
+$hsc = function(mixed $p) use ($hsc_array, $is_scalar): array|string {
     if ($is_scalar($p)) {
         return htmlspecialchars((string)$p, ENT_QUOTES, 'utf-8');
     } elseif (is_array($p)) {
@@ -132,17 +99,185 @@ $hsc = function($p) use ($hsc_array, $is_scalar): array|string {
 };
 $helpers['hsc'] = [$hsc, HELPER_RETURN_ESCAPED_DATA];
 //endregion
+//endregion
 
+//region PHPECHO INSTANCE HELPERS
+//region raw
+/**
+ * Return the raw value from the key in parameter
+ * CAREFUL : THIS VALUE IS NOT ESCAPED
+ *
+ * This helper is linked to an instance of PhpEcho
+ *
+ * Support the key space notation for array and sub-arrays
+ * if $key = 'abc def' then the engine will search for
+ * the value as $vars['abc']['def']
+ *
+ * @param string $key
+ * @return mixed
+ */
+$raw = function(string $key): mixed {
+    if (isset($this->vars[$key])) {
+        return $this->vars[$key];
+    } else {
+        $keys = explode(' ', $key);
+        $cursor = $this->vars;
+        foreach ($keys as $k) {
+            if (isset($cursor[$k])) {
+                $cursor = $cursor[$k];
+            } else {
+                return null;
+            }
+        }
+
+        return $cursor;
+    }
+};
+$helpers['raw'] = [$raw, HELPER_BOUND_TO_CLASS_INSTANCE, HELPER_RETURN_ESCAPED_DATA];
+//endregion
+
+//region key_up
+/**
+ * This helper will climb the tree of PhpEcho instances from the current block while the key match
+ *
+ * A string will be split in parts using the space for delimiter
+ * If one of the keys contains a space, use an array of keys instead
+ *
+ * If $strict_match === true then every key must be found in the parent blocks tree as they are listed
+ * If $strict_match === false then if the current key is not found in the parent block, then the search will continue
+ * until reaching the root or stop at the first match
+ *
+ * @param string|array $keys
+ * @param bool $strict_match
+ * @return mixed null if not found
+ */
+$key_up = function(array|string $keys, bool $strict_match = true) use ($to_escape, $hsc) {
+    /** @var PhpEcho $this */
+    if ( ! $this->hasParent()) {
+        return null;
+    }
+    $keys  = is_string($keys) ? explode(' ', $keys) : $keys;
+    /** @var PhpEcho $block */
+    $block = $this->parent;
+    $nb    = count($keys);
+    $i     = 0;
+    while ($i < $nb) {
+        $k = $keys[$i];
+        if (isset($block[$k])) {
+            if ($i + 1 >= $nb) {
+                if ($to_escape($block[$k])) {
+                    return $hsc($block[$k]);
+                } else {
+                    return $block[$k];
+                }
+            } else {
+                ++$i;
+            }
+        } elseif ($strict_match) {
+            return null;
+        }
+        if ($block->hasParent()) {
+            $block = $block->parent;
+        } else {
+            return null;
+        }
+    }
+};
+$helpers['keyUp'] = [$key_up, HELPER_BOUND_TO_CLASS_INSTANCE, HELPER_RETURN_ESCAPED_DATA];
+//endregion
+
+//region root
+/**
+ * @return PhpEcho
+ */
+$root = function(): PhpEcho {
+    // climbing to the root
+    /** @var PhpEcho $block */
+    $block = $this;
+    while ($block->hasParent()) {
+        $block = $block->parent;
+    }
+
+    return $block;
+};
+$helpers['root'] = [$root, HELPER_BOUND_TO_CLASS_INSTANCE, HELPER_RETURN_ESCAPED_DATA];
+//endregion
+
+//region root_var
+/**
+ * This helper will extract a value from a key stored in the root of the tree of PhpEcho instances
+ * and go down while the key match. This function does not render the PhpEcho blocks
+ *
+ * A string will be split in parts using the space for delimiter
+ * If one of the keys contains a space, use an array of keys instead
+ *
+ * @param array|string $keys
+ * @return mixed null if not found
+ */
+$root_var = function(array|string $keys) use ($to_escape, $hsc): mixed {
+    /** @var PhpEcho $this */
+    $root = $this->bound_helpers['root'];
+    /** @var PhpEcho $block */
+    $block = $root();   // get the root PhpEcho block
+    $keys = is_string($keys) ? explode(' ', $keys) : $keys;
+    $nb = count($keys);
+    $i = 0;
+    while ($i < $nb) {
+        $k = $keys[$i];
+        if (isset($block[$k])) {
+            if ($i + 1 >= $nb) {
+                if ($to_escape($block[$k])) {
+                    return $hsc($block[$k]);
+                } else {
+                    return $block[$k];
+                }
+            } else {
+                $block = $block[$k];
+                ++$i;
+            }
+        } else {
+            return null;
+        }
+    }
+};
+$helpers['rootVar'] = [$root_var, HELPER_BOUND_TO_CLASS_INSTANCE, HELPER_RETURN_ESCAPED_DATA];
+//endregion
+
+//region seek_param
+/**
+ * Seek the parameter from the current block to the root
+ *
+ * @param string $name
+ * @return null
+ */
+$seek_param = function(string $name) {
+    /** @var PhpEcho $block */
+    $block = $this;
+    while (true) {
+        if ($block->hasParam($name)) {
+            return $block->params[$name];
+        } elseif ($block->hasParent()) {
+            $block = $block->parent;
+        } else {
+            return null;
+        }
+    }
+};
+$helpers['seekParam'] = [$seek_param, HELPER_BOUND_TO_CLASS_INSTANCE, HELPER_RETURN_ESCAPED_DATA];
+//endregion
+//endregion
+
+//region HTML HELPERS
 //region selected
 /**
  * Return the html attribute "selected" if $p == $ref
  * This is a standalone helper
  *
- * @param $p value to check
- * @param $ref selected value ref
+ * @param mixed $p scalar value to check
+ * @param mixed $ref scalar value ref
  * @return string
  */
-$selected = function($p, $ref) use ($is_scalar): string {
+$selected = function(mixed $p, mixed $ref) use ($is_scalar): string {
     return $is_scalar($p) && $is_scalar($ref) && ((string)$p === (string)$ref) ? ' selected ' : '';
 };
 $helpers['selected'] = [$selected, HELPER_RETURN_ESCAPED_DATA];
@@ -153,11 +288,11 @@ $helpers['selected'] = [$selected, HELPER_RETURN_ESCAPED_DATA];
  * Return the html attribute "checked" if $p == $ref
  * This is a standalone helper
  *
- * @param $p value to check
- * @param $ref checked value ref
+ * @param mixed $p scalar value to check
+ * @param mixed $ref scalar value ref
  * @return string
  */
-$checked = function($p, $ref) use ($is_scalar): string {
+$checked = function(mixed $p, mixed $ref) use ($is_scalar): string {
     return $is_scalar($p) && $is_scalar($ref) && ((string)$p === (string)$ref) ? ' checked ' : '';
 };
 $helpers['checked'] = [$checked, HELPER_RETURN_ESCAPED_DATA];
@@ -322,135 +457,6 @@ $script = function(array $p) use ($tag): string {
 };
 $helpers['script'] = [$script, HELPER_RETURN_ESCAPED_DATA];
 //endregion
-
-//region key_up
-/**
- * This helper will climb the tree of PhpEcho instances from the current block while the key match
- *
- * A string will be split in parts using the space for delimiter
- * If one of the keys contains a space, use an array of keys instead
- *
- * If $strict_match === true then every key must be found in the parent blocks tree as they are listed
- * If $strict_match === false then if the current key is not found in the parent block, then the search will continue
- * until reaching the root or stop at the first match
- *
- * @param string|array $keys
- * @param bool $strict_match
- * @return mixed null if not found
- */
-$key_up = function(array|string $keys, bool $strict_match = true) use ($to_escape, $hsc) {
-    /** @var PhpEcho $this */
-    if ( ! $this->hasParent()) {
-        return null;
-    }
-    $keys = is_string($keys) ? explode(' ', $keys) : $keys;
-    /** @var PhpEcho $block */
-    $block = $this->parent;
-    $nb = count($keys);
-    $i = 0;
-    while ($i < $nb) {
-        $k = $keys[$i];
-        if (isset($block[$k])) {
-            if ($i + 1 >= $nb) {
-                if ($to_escape($block[$k])) {
-                    return $hsc($block[$k]);
-                } else {
-                    return $block[$k];
-                }
-            } else {
-                ++$i;
-            }
-        } elseif ($strict_match) {
-            return null;
-        }
-        if ($block->hasParent()) {
-            $block = $block->parent;
-        } else {
-            return null;
-        }
-    }
-};
-$helpers['keyUp'] = [$key_up, HELPER_BOUND_TO_CLASS_INSTANCE, HELPER_RETURN_ESCAPED_DATA];
-//endregion
-
-//region root
-/**
- * @return PhpEcho
- */
-$root = function(): PhpEcho {
-    // climbing to the root
-    /** @var PhpEcho $block */
-    $block = $this;
-    while ($block->hasParent()) {
-        $block = $block->parent;
-    }
-
-    return $block;
-};
-$helpers['root'] = [$root, HELPER_BOUND_TO_CLASS_INSTANCE, HELPER_RETURN_ESCAPED_DATA];
-//endregion
-
-//region root_var
-/**
- * This helper will extract a value from a key stored in the root of the tree of PhpEcho instances
- * and go down while the key match. This function does not render the PhpEcho blocks
- *
- * A string will be split in parts using the space for delimiter
- * If one of the keys contains a space, use an array of keys instead
- *
- * @param array|string $keys
- * @return mixed null if not found
- */
-$root_var = function(array|string $keys) use ($to_escape, $hsc): mixed {
-    /** @var PhpEcho $this */
-    $root = $this->bound_helpers['root'];
-    /** @var PhpEcho $block */
-    $block = $root();   // get the root PhpEcho block
-    $keys = is_string($keys) ? explode(' ', $keys) : $keys;
-    $nb = count($keys);
-    $i = 0;
-    while ($i < $nb) {
-        $k = $keys[$i];
-        if (isset($block[$k])) {
-            if ($i + 1 >= $nb) {
-                if ($to_escape($block[$k])) {
-                    return $hsc($block[$k]);
-                } else {
-                    return $block[$k];
-                }
-            } else {
-                $block = $block[$k];
-                ++$i;
-            }
-        } else {
-            return null;
-        }
-    }
-};
-$helpers['rootVar'] = [$root_var, HELPER_BOUND_TO_CLASS_INSTANCE, HELPER_RETURN_ESCAPED_DATA];
-//endregion
-
-//region seek_param
-/**
- * Seek the parameter from the current block to the root
- *
- * @param string $name
- * @return null
- */
-$seek_param = function(string $name) {
-    /** @var PhpEcho $block */
-    $block = $this;
-    while (true) {
-        if ($block->hasParam($name)) {
-            return $block->params[$name];
-        } elseif ($block->hasParent()) {
-            $block = $block->parent;
-        } else {
-            return null;
-        }
-    }
-};
-$helpers['seekParam'] = [$seek_param, HELPER_BOUND_TO_CLASS_INSTANCE, HELPER_RETURN_ESCAPED_DATA];
 //endregion
 
 // return the array of helpers to PhpEcho
