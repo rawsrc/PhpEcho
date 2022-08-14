932eb976d18c1c749d3607e662a2313c4d9a2476
rawsrc <rawsrc@gmail.com>
2022-08-14 20:37:26 +0200

    Tests in progress

---
+++ tests/view/Layout.php
@@ -0,0 +1,12 @@
+<?php /** @var rawsrc\PhpEcho\PhpEcho $this */ // MAIN LAYOUT ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="<?= $this['description]'] ?>">
+  <title><?= $this['title'] ?></title>
+</head>
+<body>
+<?= $this['body'] ?>
+</body>
+</html>
